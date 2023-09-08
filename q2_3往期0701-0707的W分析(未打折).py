ae9adabb9201ec974c31de2d49f8b5a294b4a5b6
738NGX <ningyue@738ngx.site>
2023-09-09 02:18:49 +0800

    2309090218

---
+++ q2_3往期0701-0707的W分析(未打折).py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df=pd.read_csv('./export/q2_2往期品类销量定价关系汇总.csv')
+
+df['日期'] = pd.to_datetime(df['日期'])
+mask = (((df['日期'] >= '2020-07-01') & (df['日期'] <= '2020-07-07')) | 
+        ((df['日期'] >= '2021-07-01') & (df['日期'] <= '2021-07-07')) |
+        ((df['日期'] >= '2022-07-01') & (df['日期'] <= '2022-07-07'))
+       )
+df = df.loc[mask]
+df=df.set_index(['日期'])
+result=pd.DataFrame(columns=['W_水生根茎类', 'W_花叶类', 'W_花菜类', 'W_茄类', 'W_辣椒类', 'W_食用菌'])
+for typ in df['分类名称'].unique():
+    df0=df[df['分类名称']==typ]
+    result[f'W_{typ}']=df0['销售单价(元/千克)_否']/df0['批发价格(元/千克)_否']-1
+    
+result.to_csv('./export/q2_3往期0701-0707的W分析(未打折).csv')
